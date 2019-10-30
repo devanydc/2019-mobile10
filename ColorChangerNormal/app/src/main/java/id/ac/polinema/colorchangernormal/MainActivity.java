abec85548e4166f45e3e7256ba08427c09aa5c67
devanydc <devany1212.dc@gmail.com>
2019-10-30 21:29:44 +0700

    Langkah Percobaan 2 has done

--- ColorChangerNormal/app/src/main/java/id/ac/polinema/colorchangernormal/MainActivity.java
+++ ColorChangerNormal/app/src/main/java/id/ac/polinema/colorchangernormal/MainActivity.java
@@ -2,18 +2,18 @@ package id.ac.polinema.colorchangernormal;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-
+import androidx.lifecycle.ViewModelProviders;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
 
-	private ConstraintLayout rootView;
-	private Button btnChangeColor;
+public class MainActivity extends AppCompatActivity {
+	ConstraintLayout rootView;
+	Button btnChangeColor;
+	ColorViewModel colorViewModel;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,23 +21,21 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 		rootView = findViewById(R.id.layout_main);
 		btnChangeColor = findViewById(R.id.change_color);
-
-		rootView.setBackgroundColor(generateRandomColor());
+		colorViewModel = ViewModelProviders.of(this).get(ColorViewModel.class);
+		rootView.setBackgroundColor(colorViewModel.getColor());
 
 		// Tambahkan event klik pada tombol
-
-		btnChangeColor.setOnClickListener(new View.OnClickListener(){
+		btnChangeColor.setOnClickListener(new View.OnClickListener() {
 			@Override
-			public void onClick(View view){
-				int color =generateRandomColor();
-				rootView.setBackgroundColor(color);
+			public void onClick(View view) {
+				int color = generateRandomColor();
+				colorViewModel.setColor(color);
+				rootView.setBackgroundColor(colorViewModel.getColor());
 			}
 		});
 	}
-
 	private int generateRandomColor(){
 		Random rnd = new Random();
 		return Color.argb(255,rnd.nextInt(256),rnd.nextInt(256),rnd.nextInt(256));
 	}
-
 }
