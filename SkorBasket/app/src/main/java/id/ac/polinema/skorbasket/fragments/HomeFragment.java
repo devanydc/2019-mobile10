315b2fc3f04d53769e77c4a5bd145ac2ee75eaff
devanydc <devany1212.dc@gmail.com>
2019-11-06 00:47:22 +0700

    Skor Basket has done but still error in 'of'

--- SkorBasket/app/src/main/java/id/ac/polinema/skorbasket/fragments/HomeFragment.java
+++ SkorBasket/app/src/main/java/id/ac/polinema/skorbasket/fragments/HomeFragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -27,6 +29,7 @@ public class HomeFragment extends Fragment {
 	private TextView scoreHome;
 	private SharedScore sharedScore;
 	private int scoreDefault = 0;
+	private Object SharedScore;
 
 	public HomeFragment() {
 		// Required empty public constructor
@@ -34,7 +37,9 @@ public class HomeFragment extends Fragment {
 
 	@Override
 	public void onCreate(@Nullable Bundle savedInstanceState) {
+
 		super.onCreate(savedInstanceState);
+		sharedScore=ViewModelProvider.of(requireActivity()).get(SharedScore.class);
 	}
 
 	@Override
@@ -53,5 +58,30 @@ public class HomeFragment extends Fragment {
 		scoreSatuHome = view.findViewById(R.id.scoreSatuHome);
 
 		// Tambahkan logic tombol di bagian bawah ini
+		sharedScore.getScoreHome().observe(requireActivity(),new Observer<Integer>(){
+			@Override
+			public void onChanged(Integer score){
+				scoreHome.setText(String.valueOf(score));
+				scoreDefault = score;
+			}
+		});
+		scoreDuaHome.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				sharedScore.setScoreHome(scoreDefault +2);
+			}
+		});
+		scoreTigaHome.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				sharedScore.setScoreHome(scoreDefault +3);
+			}
+		});
+		scoreSatuHome.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				sharedScore.setScoreHome(scoreDefault +1);
+			}
+		});
 	}
 }
--- SkorBasket/app/src/main/java/id/ac/polinema/skorbasket/viewmodels/SharedScore.java
+++ SkorBasket/app/src/main/java/id/ac/polinema/skorbasket/viewmodels/SharedScore.java
@@ -1,4 +1,45 @@
 package id.ac.polinema.skorbasket.viewmodels;
 
-public class SharedScore {
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
+
+public class SharedScore extends AndroidViewModel{
+
+    private MutableLiveData<Integer> scoreHome;
+    private MutableLiveData<Integer> scoreVisitor;
+    private MutableLiveData<Boolean> winner;
+
+    public SharedScore(@NonNull Application application){
+        super(application);
+        scoreHome =new MutableLiveData<>();
+        scoreVisitor =new MutableLiveData<>();
+        winner =new MutableLiveData<>();
+    }
+
+    public MutableLiveData<Integer> getScoreHome() {
+        return scoreHome;
+    }
+
+    public void setScoreHome(int scoreHome) {
+        this.scoreHome = scoreHome;
+    }
+
+    public MutableLiveData<Integer> getScoreVisitor() {
+        return scoreVisitor;
+    }
+
+    public void setScoreVisitor(int scoreVisitor) {
+        this.scoreVisitor = scoreVisitor;
+    }
+
+    public MutableLiveData<Boolean> getWinner() {
+        return winner;
+    }
+
+    public void setWinner(boolean winner) {
+        this.winner = winner;
+    }
 }
